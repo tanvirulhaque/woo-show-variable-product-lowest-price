0b08ad310101b6641dc7343adf0a48cbacdb9585
Tanvirul Haque <tanvirul6@users.noreply.github.com>
2020-09-22 17:19:17 +0600

    Recode & Add Filter

--- woo-show-variable-product-lowest-price.php
+++ woo-show-variable-product-lowest-price.php
@@ -1,8 +1,8 @@
 <?php
 /*
  * Plugin Name: WooCommerce Show Variable Product Lowest Price
- * Plugin URI: https://wordpress.org/
- * Description: Shows only the lowest price and sale in variable WooCommerce products.
+ * Plugin URI: https://wordpress.org/woo-show-variable-product-lowest-price/
+ * Description: Shows only the lowest price and sale price in the WooCommerce variable products.
  * Author: Tanvirul Haque
  * Version: 1.0.0
  * Author URI: http://wpxpress.net
@@ -14,31 +14,20 @@
 */
 
 function wvlp_variable_price_range( $price, $product ) {
-    $prefix = sprintf( '%s: ', __( 'From', 'woo-show-variable-product-lowest-price' ) );
+    $prefix = apply_filters( 'wvlp_price_title', sprintf( '%s: ', __( 'From', 'woo-show-variable-product-lowest-price' ) ) );
 
-    $min_var_reg_price = $product->get_variation_regular_price( 'min', true );
-    $max_var_reg_price = $product->get_variation_regular_price( 'max', true );
-
+    $min_var_reg_price  = $product->get_variation_regular_price( 'min', true );
+    $max_var_reg_price  = $product->get_variation_regular_price( 'max', true );
     $min_var_sale_price = $product->get_variation_sale_price( 'min', true );
     $max_var_sale_price = $product->get_variation_sale_price( 'max', true );
+    $min_price          = $product->get_variation_price( 'min', true );
+    $max_price          = $product->get_variation_price( 'max', true );
 
-    $min_price = $product->get_variation_price( 'min', true );
-    $max_price = $product->get_variation_price( 'max', true );
-
-
-    // Solution 1
-   if ( $product->is_on_sale() ) {
-       $price = sprintf( '%1$s <del>%2$s</del> <ins>%3$s</ins>', $prefix, wc_price( $min_var_reg_price ), wc_price( $min_var_sale_price ) );
-   } else {
-       $price = sprintf( '%1$s %2$s', $prefix, wc_price( $min_var_reg_price ) );
-   }
-
-    // Solution 2
-    $wvlp_price = ( $min_var_sale_price == $min_var_reg_price ) ? wc_price( $min_var_reg_price ) : '<del>' . wc_price( $min_var_reg_price ) . '</del> ' . '<ins class="highlight">' . wc_price( $min_var_sale_price ) . '</ins>';
-    // $price      = ( $max_price == $min_price ) ? $wvlp_price : sprintf( '%s%s', $prefix, $wvlp_price );
+    $price = ( $product->is_on_sale() ) ? sprintf( '%1$s <del>%2$s</del> <ins>%3$s</ins>', $prefix, wc_price( $min_var_reg_price ), wc_price( $min_var_sale_price ) ) : sprintf( '%1$s %2$s', $prefix, wc_price( $min_var_reg_price ) );
 
     return $price;
 }
 
 add_filter( 'woocommerce_variable_sale_price_html', 'wvlp_variable_price_range', 10, 2 );
 add_filter( 'woocommerce_variable_price_html', 'wvlp_variable_price_range', 10, 2 );
+
